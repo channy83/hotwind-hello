f50bf928e8558faeb833932e0b389807a69c2316
jhyun <jhyun@mainpc>
2012-08-30 10:13:14 +0900

    initial commit

---
+++ src/main/java/jhyun/mybatis_with_guice/App.java
@@ -0,0 +1,13 @@
+package jhyun.mybatis_with_guice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
