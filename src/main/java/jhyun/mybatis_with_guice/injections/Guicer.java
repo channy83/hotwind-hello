8d7d5c7ada43ff271039096a75de2347ba65db83
jhyun <jhyun@mainpc>
2012-08-30 15:46:04 +0900

    guicer decoupled + configurable

--- src/main/java/jhyun/mybatis_with_guice/injections/Guicer.java
+++ src/main/java/jhyun/mybatis_with_guice/injections/Guicer.java
@@ -1,7 +1,17 @@
 package jhyun.mybatis_with_guice.injections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jhyun.mybatis_with_guice.config.AppConfig;
+
+import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Module;
 
 /**
  * Guice Injector.
@@ -10,10 +20,32 @@ import com.google.inject.Injector;
  * @since 2012-08-30
  */
 public class Guicer {
+	private static Logger logger = LoggerFactory.getLogger(Guicer.class);
+
 	private static Injector injector;
 
 	static {
-		injector = Guice.createInjector(new MyBatisXmlModule());
+		//
+		Configuration config = AppConfig.load();
+		List<Object> moduleClassFqns = config.getList("app.guice-modules",
+				new ArrayList<Object>());
+		logger.debug(String.format("guice-module-class-fqns = %s",
+				moduleClassFqns));
+		//
+		List<Module> moduleObjs = new ArrayList<Module>();
+		for (Object moduleClassFqn : moduleClassFqns) {
+			try {
+				Class<? extends Module> moduleClass = (Class<? extends Module>) Class
+						.forName((String) moduleClassFqn);
+				Module moduleObj = moduleClass.newInstance();
+				moduleObjs.add(moduleObj);
+			} catch (Exception e) {
+				logger.warn(String
+						.format("app.guice-modules specified guice-module-class not found.",
+								e));
+			}
+		}
+		injector = Guice.createInjector(moduleObjs);
 	}
 
 	/** use this! */
