f898e531fcba792f037bd27853210a148975d414
jhyun <jhyun@mainpc>
2012-08-30 15:05:09 +0900

    ...

--- src/test/java/jhyun/mybatis_with_guice/tests/test_cases/MybatisWithGuiceTestCase.java
+++ src/test/java/jhyun/mybatis_with_guice/tests/test_cases/MybatisWithGuiceTestCase.java
@@ -1,21 +1,29 @@
 package jhyun.mybatis_with_guice.tests.test_cases;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import jhyun.mybatis_with_guice.injections.Guicer;
 import jhyun.mybatis_with_guice.sqlmaps.HelloMapper;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import com.google.inject.Injector;
+
 public class MybatisWithGuiceTestCase {
+
+	private Injector getInjector() {
+		return Guicer.get();
+	}
 
 	@Test
 	public void testInjectedSqlSession() {
-		assertNotNull(Guicer.get().getInstance(SqlSession.class));
+		assertNotNull(getInjector().getInstance(SqlSession.class));
 	}
 
+	@Test
 	public void testSqlMapper() {
-		HelloMapper helloMapper = Guicer.get().getInstance(HelloMapper.class);
+		HelloMapper helloMapper = getInjector().getInstance(HelloMapper.class);
 		assertNotNull(helloMapper);
 		//
 		assertEquals(3, helloMapper.plus(1, 2));
