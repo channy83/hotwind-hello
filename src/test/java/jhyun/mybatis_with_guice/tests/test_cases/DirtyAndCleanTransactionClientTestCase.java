84fc4f20e56cfce8ddbd94f92cd5bbbd009218b5
jhyun <jhyun@mainpc>
2012-09-03 11:29:19 +0900

    transaction test-cases.

--- src/test/java/jhyun/mybatis_with_guice/tests/test_cases/DirtyAndCleanTransactionClientTestCase.java
+++ src/test/java/jhyun/mybatis_with_guice/tests/test_cases/DirtyAndCleanTransactionClientTestCase.java
@@ -1,12 +1,24 @@
 package jhyun.mybatis_with_guice.tests.test_cases;
 
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+
 import jhyun.mybatis_with_guice.injections.Guicer;
+import jhyun.mybatis_with_guice.tests.helpers.JerseyTestingClient;
 import jhyun.mybatis_with_guice.tests.helpers.SimpleTransactionHelper;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.google.inject.Inject;
 
+/**
+ * 병합 트랜잭션 처리가 잘되는지 안되는지에 대한 테스트.
+ * 
+ * @author jhyun
+ * @since 2012-09-03
+ */
 public class DirtyAndCleanTransactionClientTestCase {
 
 	public DirtyAndCleanTransactionClientTestCase() {
@@ -17,14 +29,36 @@ public class DirtyAndCleanTransactionClientTestCase {
 	@Inject
 	private SimpleTransactionHelper sth;
 
+	/** 트랜잭션 처리 제대로 안되는 예시 */
 	@Test
 	public void testSomeDirtyTransactions() {
-		// TODO:
+		sth.resetTable();
+		try {
+			JerseyTestingClient.webResource()
+					.path("/hello/transaction/addThreeDirty")
+					.accept(MediaType.TEXT_PLAIN).get(String.class);
+			fail("FAIL!!! API SHOULD THROW SOME EXCEPTION!!!");
+		} catch (Exception exc) {
+			List l = sth.listAll();
+			// NOTE: 트랜잭션 취소 처리가 안되고, 잔여 3개가 남아있어야함.
+			assertEquals(3, l.size());
+		}
 	}
 
+	/** 정상적인 트랜잭션 처리 예시 */
 	@Test
 	public void testSomeCleanTransactions() {
-		// TODO:
+		sth.resetTable();
+		try {
+			JerseyTestingClient.webResource()
+					.path("/hello/transaction/addThreeCleanly")
+					.accept(MediaType.TEXT_PLAIN).get(String.class);
+			fail("FAIL!!! API SHOULD THROW SOME EXCEPTION!!!");
+		} catch (Exception exc) {
+			List l = sth.listAll();
+			// NOTE: 예외가 발생했고, @Transactional이 정상적으로 적용되었다면, 테이블에는 아무것도 없어야함.
+			assertEquals(0, l.size());
+		}
 	}
 
 }
