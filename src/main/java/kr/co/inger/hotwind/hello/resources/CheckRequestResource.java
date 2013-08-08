2388972519e145874864311c5f9485fa9acb48bb
jonghyouk yun <ageldama@gmail.com>
2013-08-08 20:43:18 +0900

    check-request-hello working...

--- src/main/java/kr/co/inger/hotwind/hello/resources/CheckRequestResource.java
+++ src/main/java/kr/co/inger/hotwind/hello/resources/CheckRequestResource.java
@@ -1,12 +1,20 @@
 package kr.co.inger.hotwind.hello.resources;
 
+import java.util.UUID;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import kr.co.inger.hotwind.jaxrs.JaxRsResource;
+import kr.co.inger.hotwind.request_check.backend.RequestCheckKvStore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.inject.Inject;
 import com.google.inject.servlet.RequestScoped;
 
 /**
@@ -23,5 +31,40 @@ public class CheckRequestResource {
 
 	private Logger logger = LoggerFactory.getLogger(CheckRequestResource.class);
 
+	@Inject
+	private RequestCheckKvStore requestCheckKvStore;
+
+	@GET
+	@Path("/login")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String login() {
+		final String k = UUID.randomUUID().toString();
+		requestCheckKvStore.store(k, "true");
+		return k;
+	}
+
+	@GET
+	@Path("/logout/{accessToken}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String logout(@PathParam("accessToken") String accessToken) {
+		if (requestCheckKvStore.forget(accessToken)) {
+			return "OK";
+		} else {
+			return "FAIL";
+		}
+	}
+
+	@GET
+	@Path("/isLoggedIn/{accessToken}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String isLoggedIn(@PathParam("accessToken") String accessToken) {
+		if (requestCheckKvStore.isExists(accessToken)) {
+			return "LOGGED-IN";
+		} else {
+			return "NOT-LOGGED-IN";
+		}
+	}
+
+	// TODO: should-logged-in
 
 }
